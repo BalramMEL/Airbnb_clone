87b6bde603093ab5e968867ad106d0b072c60bf0
Balram Uddhav Melkunde <102988461+BalramMEL@users.noreply.github.com>
2023-04-07 23:56:56 +0530

    navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import './globals.css'
 import { Nunito } from "next/font/google"
+import Navbar from "./components/navbar/Navbar"
+import ClientOnly from './components/ClientOnly'
 
 export const metadata = {
   title: 'Airbnb',
@@ -18,7 +20,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={font.className}>{children}</body>
+      <body className={font.className}>
+        <ClientOnly>
+          <Navbar />
+        </ClientOnly>
+        {children}
+      </body>
     </html>
   )
 }
