f40c30bc37fd7cf241bcd87f5f87c299f43945ae
Balram Uddhav Melkunde <102988461+BalramMEL@users.noreply.github.com>
2023-04-19 02:28:31 +0530

    social login(github, google)

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,14 +1,21 @@
 'use client';
 
+
 import Container from "../Container"
 import Logo from "./Logo"
 import Search from "./Search"
 import UserMenu from "./UserMenu"
+import { SafeUser } from "@/app/types";
 
+interface NavbarProps{
+  currentUser?: SafeUser | null; 
+}
 
-const Navbar = () => {
-    return (
-      
+const Navbar: React.FC<NavbarProps> = ({
+  currentUser
+}) => {
+  
+    return (      
     <div className="fixed w-full bg-white z-10 shadow-sm">
         <div className="py-4 border-b-[1px]">
               <Container>
@@ -23,7 +30,7 @@ const Navbar = () => {
                   ">
                       <Logo />
                         <Search />   
-                      <UserMenu />  
+                      <UserMenu currentUser={currentUser} />  
                    </div>
               </Container>            
               
