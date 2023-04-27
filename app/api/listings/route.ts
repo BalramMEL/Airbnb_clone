2f6b37c4b612e81a8d3da175a3d765ce71bab460
Balram Uddhav Melkunde <102988461+BalramMEL@users.noreply.github.com>
2023-04-27 17:35:04 +0530

    Fetching listings with server components (Listing card component, direct server action)

--- app/api/listings/route.ts
+++ app/api/listings/route.ts
@@ -4,47 +4,47 @@ import prisma from "@/app/libs/prismadb";
 import getCurrentUser from "@/app/actions/getCurrentUser";
 
 export async function POST(
-    request: Request,
+  request: Request, 
 ) {
-    const currentUser = await getCurrentUser();
+  const currentUser = await getCurrentUser();
 
-    if (!currentUser) {
-        return NextResponse.error();
+  if (!currentUser) {
+    return NextResponse.error();
+  }
+
+  const body = await request.json();
+  const { 
+    title,
+    description,
+    imageSrc,
+    category,
+    roomCount,
+    bathroomCount,
+    guestCount,
+    location,
+    price,
+   } = body;
+
+  Object.keys(body).forEach((value: any) => {
+    if (!body[value]) {
+      NextResponse.error();
     }
+  });
 
-    const body = await request.json();
-    const {
-        title,
-        description,
-        imageSrc,
-        category,
-        roomCount,
-        bathroomCount,
-        guestCount,
-        location,
-        price,
-    } = body;
+  const listing = await prisma.listing.create({
+    data: {
+      title,
+      description,
+      imageSrc,
+      category,
+      roomCount,
+      bathroomCount,
+      guestCount,
+      locationValue: location.value,
+      price: parseInt(price, 10),
+      userId: currentUser.id
+    }
+  });
 
-    Object.keys(body).forEach((value: any) => {
-        if (!body[value]) {
-            NextResponse.error();
-        }
-    });
-
-    const listing = await prisma.listing.create({
-        data: {
-            title,
-            description,
-            imageSrc,
-            category,
-            roomCount,
-            bathroomCount,
-            guestCount,
-            locationValue: location.value,
-            price: parseInt(price, 10),
-            userId: currentUser.id
-        }
-    });
-
-    return NextResponse.json(listing);
+  return NextResponse.json(listing);
 }
